78b0ed280dd92d4d7a9c36e941b913ad31897c7e
Jekfer <bichon.jekfer@gmail.com>
2019-08-28 01:24:01 +0200

    added damage items to damage dist

--- GW2EIParser/Builders/JsonModels/JsonActors/JsonActorUtility/JsonDamageDistData.cs
+++ GW2EIParser/Builders/JsonModels/JsonActors/JsonActorUtility/JsonDamageDistData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GW2EIParser.EIData;
 using GW2EIParser.Parser;
+using GW2EIParser.Parser.ParsedData.CombatEvents;
 using static GW2EIParser.Builders.JsonModels.JsonLog;
 
 namespace GW2EIParser.Builders.JsonModels
@@ -10,6 +12,111 @@ namespace GW2EIParser.Builders.JsonModels
     /// </summary>
     public class JsonDamageDistData
     {
+        public class JsonDamageItem
+        {
+            private const long Condi = 1;
+            private const long IsOverNinety = 1 << 1;
+            private const long AgainstUnderFifty = 1 << 2;
+            private const long IsMoving = 1 << 3;
+            private const long IsFlanking = 1 << 4;
+            private const long HasHit = 1 << 5;
+            private const long HasCrit = 1 << 6;
+            private const long HasGlanced = 1 << 7;
+            private const long IsBlind = 1 << 8;
+            private const long IsAbsorbed = 1 << 9;
+            private const long HasInterrupted = 1 << 10;
+            private const long HasDowned = 1 << 11;
+            private const long HasKilled = 1 << 12;
+            private const long IsBlocked = 1 << 13;
+            private const long IsEvaded = 1 << 14;
+
+            public string Id { get; set; }
+
+            public long Time { get; set; }
+
+            public int Damage { get; set; }
+            public int ShieldDamage { get; set; }
+
+            public long EncodedBooleans { get; set; }
+
+            public JsonDamageItem(AbstractDamageEvent evt, ParsedLog log)
+            {
+                if (evt is NonDirectDamageEvent)
+                {
+                    Id = "b" + evt.SkillId;
+                }
+                else
+                {
+                    Id = "s" + evt.SkillId;
+                }
+                Time = evt.Time;
+                Damage = evt.Damage;
+                ShieldDamage = evt.ShieldDamage;
+                EncodedBooleans = 0;
+                if (evt.IsCondi(log))
+                {
+                    EncodedBooleans |= Condi;
+                }
+                if (evt.IsOverNinety)
+                {
+                    EncodedBooleans |= IsOverNinety;
+                }
+                if (evt.AgainstUnderFifty)
+                {
+                    EncodedBooleans |= AgainstUnderFifty;
+                }
+                if (evt.IsMoving)
+                {
+                    EncodedBooleans |= IsMoving;
+                }
+                if (evt.IsFlanking)
+                {
+                    EncodedBooleans |= IsFlanking;
+                }
+                if (evt.HasHit)
+                {
+                    EncodedBooleans |= HasHit;
+                }
+                if (evt.HasCrit)
+                {
+                    EncodedBooleans |= HasCrit;
+                }
+                if (evt.HasGlanced)
+                {
+                    EncodedBooleans |= HasGlanced;
+                }
+                if (evt.IsBlind)
+                {
+                    EncodedBooleans |= IsBlind;
+                }
+                if (evt.IsAbsorbed)
+                {
+                    EncodedBooleans |= IsAbsorbed;
+                }
+                if (evt.HasInterrupted)
+                {
+                    EncodedBooleans |= HasInterrupted;
+                }
+                if (evt.HasDowned)
+                {
+                    EncodedBooleans |= HasDowned;
+                }
+                if (evt.HasKilled)
+                {
+                    EncodedBooleans |= HasKilled;
+                }
+                if (evt.IsBlocked)
+                {
+                    EncodedBooleans |= IsBlocked;
+                }
+                if (evt.IsEvaded)
+                {
+                    EncodedBooleans |= IsEvaded;
+                }
+            }
+        }
+
+
         /// <summary>
         /// Total Damage distribution array \n
         /// Length == # of phases
@@ -35,6 +142,10 @@ namespace GW2EIParser.Builders.JsonModels
         /// <seealso cref="JsonDamageDist"/>
         public List<List<List<JsonDamageDist>>> TargetDamageTakenDists { get; set; } = new List<List<List<JsonDamageDist>>>();
 
+        public List<JsonDamageItem> DamageEvents { get; set; }
+
+        public List<JsonDamageItem> DamageTakenEvents { get; set; }
+
         public JsonDamageDistData(ParsedLog log, AbstractSingleActor actor, Dictionary<string, Desc> description, IEnumerable<AbstractSingleActor> targets)
         {
             List<PhaseData> phases = log.FightData.GetPhases(log);
@@ -42,7 +153,7 @@ namespace GW2EIParser.Builders.JsonModels
             TotalDamageTakenDists = new List<List<JsonDamageDist>>();
             foreach (PhaseData phase in phases)
             {
-                TotalDamageDists.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetDamageLogs(null, log, phase.Start, phase.End), log, description));
+                TotalDamageDists.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetJustActorDamageLogs(null, log, phase.Start, phase.End), log, description));
                 TotalDamageTakenDists.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetDamageTakenLogs(null, log, phase.Start, phase.End), log, description));
             }
             foreach (AbstractSingleActor target in targets)
@@ -51,7 +162,7 @@ namespace GW2EIParser.Builders.JsonModels
                 var TargetDamageTakenDist = new List<List<JsonDamageDist>>();
                 foreach (PhaseData phase in phases)
                 {
-                    TargetDamageDist.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetDamageLogs(target, log, phase.Start, phase.End), log, description));
+                    TargetDamageDist.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetJustActorDamageLogs(target, log, phase.Start, phase.End), log, description));
                     TargetDamageTakenDist.Add(JsonDamageDist.BuildJsonDamageDists(actor.GetDamageTakenLogs(target, log, phase.Start, phase.End), log, description));
                 }
                 TargetDamageDists.Add(TargetDamageDist);
@@ -65,6 +176,9 @@ namespace GW2EIParser.Builders.JsonModels
             {
                 TargetDamageTakenDists = null;
             }
+            //
+            DamageEvents = actor.GetJustActorDamageLogs(null, log, 0, log.FightData.FightDuration).Select(x => new JsonDamageItem(x, log)).ToList();
+            DamageTakenEvents = actor.GetDamageTakenLogs(null, log, 0, log.FightData.FightDuration).Select(x => new JsonDamageItem(x, log)).ToList();
         }
     }
 }
