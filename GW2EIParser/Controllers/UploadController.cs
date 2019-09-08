cadfe47bc5ed8d9364fe327c2b8d168c8c7fb3c8
Jekfer <bichon.jekfer@gmail.com>
2019-09-09 00:34:34 +0300

    removed useless method

--- GW2EIParser/Controllers/UploadController.cs
+++ GW2EIParser/Controllers/UploadController.cs
@@ -20,47 +20,6 @@ namespace GW2EIParser.Controllers
             return UploadToDPSR(fi, "https://dps.report/uploadContent?generator=rh");
 
         }
-        private static string UploadRaidar(FileInfo fi)
-        {
-            //string fileName = fi.Name;
-            //byte[] fileContents = File.ReadAllBytes(fi.FullName);
-            //Uri webService = new Uri(@"https://www.gw2raidar.com/api/v2/encounters/new");
-            //HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, webService);
-            //requestMessage.Headers.ExpectContinue = false;
-
-            //MultipartFormDataContent multiPartContent = new MultipartFormDataContent("----MyGreatBoundary");
-            //ByteArrayContent byteArrayContent = new ByteArrayContent(fileContents);
-            //byteArrayContent.Headers.Add("Content-Type", "application/octet-stream");
-            //multiPartContent.Add(byteArrayContent, "file", fileName);
-            //requestMessage.Content = multiPartContent;
-
-            //HttpClient httpClient = new HttpClient();
-            //try
-            //{
-            //    Task<HttpResponseMessage> httpRequest = httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
-            //    HttpResponseMessage httpResponse = httpRequest.Result;
-            //    HttpStatusCode statusCode = httpResponse.StatusCode;
-            //    HttpContent responseContent = httpResponse.Content;
-
-            //    if (responseContent != null)
-            //    {
-            //        Task<String> stringContentsTask = responseContent.ReadAsStringAsync();
-            //        String stringContents = stringContentsTask.Result;
-            //        int first = stringContents.IndexOf('{');
-            //        int length = stringContents.LastIndexOf('}') - first + 1;
-            //        String JSONFormat = stringContents.Substring(first, length);
-            //        DPSReportsResponseItem item = JsonConvert.DeserializeObject<DPSReportsResponseItem>(JSONFormat);
-            //        String logLink = item.permalink;
-            //        return logLink;
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    return ex.Message;
-            //    // Console.WriteLine(ex.Message);
-            //}
-            return "";
-        }
         private class DPSReportsResponseItem
         {
             public string Permalink { get; set; }
